f2cbaa71c923f3fe6bfcce32f193b45561b22835
daveperry84 <daveperry84@hotmail.co.uk>
2018-02-02 16:45:04 +0000

    add promise to service

--- public/javascripts/services/movie-service.js
+++ public/javascripts/services/movie-service.js
@@ -1,13 +1,16 @@
 angular.module('movieApp')
-    .service('MovieService', ['$http', function ($http) {
+    .service('MovieService', ['$http', '$q', function ($http, $q) {
     var _priv = {
         apiKey: 'd005bb7a',
         getMovieById: function(id) {
+            var defer = $q.defer();            
             var url = 'https://www.omdbapi.com/?i=' + id + '&apikey=' + _priv.apiKey;
 
             $http.get(url).then(function(data) {
-                return data.data;
+                defer.resolve(data.data);
             });
+            
+            return defer.promise;
         }
     }
     
