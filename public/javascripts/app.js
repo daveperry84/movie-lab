fcb48b8956c55eb13b8a0086c012c5f687fd3553
daveperry84 <daveperry84@hotmail.co.uk>
2018-02-02 15:19:53 +0000

    remove details controller from app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,5 @@
 window.movieApp = angular.module('movieApp',['ngRoute']);
 
-movieApp.controller("movieDetailsController", function ($scope, $routeParams) {
-    $scope.getMovieById($routeParams.imdbId);
-});
-
 movieApp.filter('capitalize', function() {
 	return function(s) {
 		return (angular.isString(s) && s.length > 0) ? s[0].toUpperCase() + s.substr(1).toLowerCase() : s;
