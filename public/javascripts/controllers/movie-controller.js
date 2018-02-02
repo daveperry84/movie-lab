faf8a0f09ee5d611a41d25e56df67991f886630a
daveperry84 <daveperry84@hotmail.co.uk>
2018-02-02 16:23:53 +0000

    correction to controller

--- public/javascripts/controllers/movie-controller.js
+++ public/javascripts/controllers/movie-controller.js
@@ -1,4 +1,4 @@
-movieApp.controller('movieController', ['$scope', '$http', 'MovieService' function($scope, $http, MovieService) {
+movieApp.controller('movieController', ['$scope', '$http', 'MovieService', function($scope, $http, MovieService) {
     $scope.movies = [];
 	$scope.currMovie = null;
 	$scope.currentPage = 1;
