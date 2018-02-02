d8328936c5ae45aff99b99037d8236e040ade5d7
daveperry84 <daveperry84@hotmail.co.uk>
2018-02-02 13:30:25 +0000

    update to router

--- public/javascripts/router/router.js
+++ public/javascripts/router/router.js
@@ -2,7 +2,7 @@ movieApp.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'tmpl/home.html',
-            controller: 'movieController'
+            controller: 'javascripts/movie-controller.js'
         }).when('/movie/:imdbId', {
             templateUrl: 'tmpl/movie.html',
             controller: 'movieDetailsController'
