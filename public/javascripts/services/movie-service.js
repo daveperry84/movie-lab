60f34f900f2d36d79ceb341d9677f5f5a8acb128
daveperry84 <daveperry84@hotmail.co.uk>
2018-02-02 16:03:59 +0000

    New service

--- public/javascripts/services/movie-service.js
+++ public/javascripts/services/movie-service.js
@@ -1,5 +1,19 @@
-angular.module('MovieService', [])
-.service('MovieService', function () {
+movieApp.service('MovieService', function () {
+    var _priv = {
+        apiKey: '',
+        getMovieById: function(id) {
+            var url = 'https://www.omdbapi.com/?i=' + id + '&apikey=' + _priv.apiKey;
+
+            $http.get(url).then(function(data) {
+                return data.data;
+            });
+        }
+    }
     
+    return {
+        getMovieById: function(id) {
+            return _priv.getMovieById(id);
+        }
+    }
 
 });
