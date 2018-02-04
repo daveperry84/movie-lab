38385a2b158624cefe3b072af51809e47022546a
daveperry84 <daveperry84@hotmail.co.uk>
2018-02-04 12:51:09 +0000

    Update search controller

--- public/javascripts/controllers/movie-search-controller.js
+++ public/javascripts/controllers/movie-search-controller.js
@@ -11,20 +11,6 @@ movieApp.controller('movieSearchController', ['$scope', '$http', 'MovieService',
     $scope.yearInvalid = false;
     $scope.invalidYearText = "Invalid input";
 
-    $scope.getTodaysDate = function () {
-        var today = new Date();
-        var date = today.getDate().toString();
-
-        var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-        var daySuffix = (date.substr(date.length-1,1) === "1") ? "st" :
-            (date.substr(date.length-1,1) === "2") ? "nd" :
-                (date.substr(date.length-1,1) === "3") ? "rd" : "th";
-
-        return day[today.getDay()] + ' ' + date + daySuffix + ' ' + month[today.getMonth()] + ' ' + today.getFullYear().toString();
-    };
-
     $scope.validateFormAndSearch = function() {
         $scope.titleInvalid = !$scope.searchParams.currentTitle || $scope.searchParams.currentTitle === null || $scope.searchParams.currentTitle === "";
 
