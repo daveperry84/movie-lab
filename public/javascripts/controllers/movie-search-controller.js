42d143933fcf51b02acd46308ce13ff76c048d88
daveperry84 <daveperry84@hotmail.co.uk>
2018-05-03 15:52:30 +0100

    Update movie-search-controller.js

--- public/javascripts/controllers/movie-search-controller.js
+++ public/javascripts/controllers/movie-search-controller.js
@@ -1,87 +1,61 @@
 movieApp.controller('movieSearchController', ['$scope', '$http', 'MovieService', function($scope, $http, MovieService) {
     $scope.movies = [];
-    $scope.currMovie = null;
-    $scope.lastPage = null;
     $scope.searchParams = {};
-    $scope.noOfResults = 0;
-    $scope.firstItemNo = null;
-    $scope.lastItemNo = null;
-    $scope.zeroResults = false;
-    $scope.titleInvalid = false;
-    $scope.yearInvalid = false;
-    $scope.invalidYearText = "Invalid input";
+    $scope.results = {
+	noOfResults: 0,
+	zeroResults: false,
+	firstItemNo: null,
+	lastItemNo: null,
+	currentPage: null,
+	lastPage: null
+    }
 	
     var initialise = function() {	    
 	    if(sessionStorage.getItem('searchParams')) {
-		$scope.searchParams = JSON.parse(sessionStorage.getItem('searchParams')); 
-		searchMovies();
+			$scope.searchParams = JSON.parse(sessionStorage.getItem('searchParams')); 
+			searchMovies();
 	    } 
     }
-
-    $scope.validateFormAndSearch = function() {
-        $scope.titleInvalid = !$scope.searchParams.currentTitle || $scope.searchParams.currentTitle === null || $scope.searchParams.currentTitle === "";
-
-        if(!isNaN(parseInt($scope.searchParams.currentYear))) {
-            var today = new Date();
-            var thisYear = today.getFullYear();
-
-            $scope.yearInvalid = parseInt($scope.searchParams.currentYear) < 1890 || parseInt($scope.searchParams.currentYear) > thisYear+1;
-            $scope.invalidYearText = "Must be in range 1890-" + thisYear;
-        } else {
-            $scope.yearInvalid = $scope.searchParams.currentYear && $scope.searchParams.currentYear !== null && $scope.searchParams.currentYear !== "";;
-        }
-
-        if(!$scope.titleInvalid && !$scope.yearInvalid) {
-	    $scope.searchParams.currentPage = 1;
-            searchMovies();
-        }
-    }
 	
-	var searchMovies = function() {		
+	var searchMovies = function(event) {
 		MovieService.searchMovies($scope.searchParams).then(function(data) {
-			if(!!data.Search) {
-				$scope.movies = data.Search;
-			}
+			$scope.movies = !!data.Search ? data.Search : [];
 
 			if($scope.movies.length > 0) {
-				$scope.zeroResults = false;
-				$scope.noOfResults = data.totalResults;
-				$scope.lastPage = Math.ceil($scope.noOfResults/10);
-				getItemNumbers($scope.searchParams.currentPage, $scope.noOfResults);
+				$scope.results.zeroResults = false;
+				$scope.results.noOfResults = data.totalResults;
+				$scope.results.lastPage = Math.ceil($scope.results.noOfResults/10);
+				$scope.results.currentPage = $scope.searchParams.currentPage;
+				getItemNumbers($scope.results.currentPage, $scope.results.noOfResults);
 			} else {
-				$scope.zeroResults = true;
+				$scope.results.zeroResults = true;
 			}
-		});
-	};
-	
-	$scope.nextPage = function() {
-		$scope.searchParams.currentPage += 1;
-		
-		MovieService.searchMovies($scope.searchParams).then(function(data) {
-			$scope.movies = data.Search;
-			getItemNumbers($scope.searchParams.currentPage, $scope.noOfResults);
-		});
-	};
-	
-	$scope.prevPage = function() {
-		$scope.searchParams.currentPage -= 1;
-		
-		MovieService.searchMovies($scope.searchParams).then(function(data) {
-			$scope.movies = data.Search;
-			getItemNumbers($scope.searchParams.currentPage, $scope.noOfResults);
 		});
 	};
 
 	var getItemNumbers = function(page, totalResults) {
-		$scope.firstItemNo = ((page - 1) * 10) + 1;
+		$scope.results.firstItemNo = ((page - 1) * 10) + 1;
 		var lastItem = page * 10;
 
 		if(totalResults < lastItem || totalResults === lastItem) {
-			$scope.lastItemNo = totalResults;
+			$scope.results.lastItemNo = totalResults;
 		} else {
-			$scope.lastItemNo = lastItem;
+			$scope.results.lastItemNo = lastItem;
 		}
 	}
+
+	var changePage = function(event, page) {
+		$scope.searchParams.currentPage += page.direction;
+                
+		MovieService.searchMovies($scope.searchParams).then(function(data) {
+			$scope.movies = data.Search;
+			$scope.results.currentPage = $scope.searchParams.currentPage;
+			getItemNumbers($scope.results.currentPage, $scope.results.noOfResults);
+		});
+	}
+
+	$scope.$on('searchMovies', searchMovies);
+	$scope.$on('changePage', changePage);
 	
 	initialise();
 }]);
